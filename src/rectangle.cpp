55793a4727a9250735dcc0a47b9576ee04b2defa
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-12 15:37:31 -0700

    Create rectangle.cpp

---
+++ src/rectangle.cpp
@@ -0,0 +1,25 @@
+#include "rectangle.h"
+#include <iostream>
+
+Rectangle::Rectangle(Point center, std::string name, int width, int height)
+    : Shape(center, name), width(width), height(height) {}
+
+double Rectangle::area() const {
+    return width * height;
+}
+
+void Rectangle::draw(std::ostream& out) const {
+        
+    for (int i = 0; i < height; i += 2)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            out << '*';
+        }
+        out << '\n';
+    }
+}
+
+Rectangle* Rectangle::clone() const {
+    return new Rectangle(*this);
+}
