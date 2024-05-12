dbaf54f7c0582533933b37252783ae083df95b88
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-12 15:39:34 -0700

    Create square.hpp

---
+++ src/square.hpp
@@ -0,0 +1,16 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+
+#include "rectangle.hpp"
+
+class Square : public Rectangle {
+public:
+    Square(Point center, std::string name, int side);
+
+    Square* clone() const override;
+
+protected:
+    Square(const Square& other) = default;
+};
+
+#endif
